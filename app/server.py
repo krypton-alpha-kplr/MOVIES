9db285b54860158751d7e0d3a82c908376ea4ce5
krypton alpha <krypton.alpha@training.kplr.fr>
2023-07-07 15:18:50 +0000

    sfjsfjsj

--- app/server.py
+++ app/server.py
@@ -13,30 +13,39 @@ import os
 # cheroot.wsgi pour le serveur WSGI CherryPy.
 import cheroot.wsgi
 # SparkContext et SparkConf pour travailler avec Spark.
-#from pyspark.context import SparkContext
-from pyspark import SparkConf
-# SparkSession pour créer une session Spark.
-sc = SparkContext('local')
-spark = SparkSession(sc) #Acceder aux donnees parralélisées
+from pyspark.context import SparkContext
+import SparkConf
+
 # create_app (supposons qu'il s'agit d'un fichier app.py) pour créer l'application Flask.
-import app.py
-#from app import create_app
+# import app.py
+from app import create_app
+
+from pyspark.sql import SparkSession
 
 #Créez un objet SparkConf :
 conf = SparkConf().setAppName("movie_recommendation-server")
 
-# 3. Initialisez le contexte Spark :
+# 3. Initialisez le contexte Spark & charger librairies:
 sc = SparkContext(conf=conf, pyFiles=['engine.py', 'app.py'])
 
+# SparkSession pour créer une session Spark.
+spark = SparkSession(sc) #Acceder aux donnees parralélisées
+
 # Obtenez les chemins des jeux de données des films et des évaluations à partir des arguments de la ligne de commande :
-movies_set_path = sys.argv[1] if len(sys.argv) > 1 else "" ratings_set_path = sys.argv[2] if len(sys.argv) > 2 else ""
+movies_set_path = sys.argv[1] if len(sys.argv) > 1 else ""
+ratings_set_path = sys.argv[2] if len(sys.argv) > 2 else ""
 
 # Créez l'application Flask :
 app = create_app(sc, movies_set_path, ratings_set_path)
+
 # Configurez et démarrez le serveur CherryPy :
-cherrypy.tree.graft(app.wsgi_app, '/') cherrypy.config.update({'server.socket_host': '0.0.0.0',
+cherrypy.tree.graft(app.wsgi_app, '/')
+cherrypy.config.update({'server.socket_host': '0.0.0.0',
                          'server.socket_port': 5432,
                          'engine.autoreload.on': False
-                         }) cherrypy.engine.start()
+                         })
+
+if __name__ == '__main__':
+    cherrypy.engine.start()
 
 #Spark-movie-recommendation-md/03_Creation_de_server.py.md at main · ayoub
